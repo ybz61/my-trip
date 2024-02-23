c113020a6f152cf1d9cf74e8b5dcc1e405e5d289
yaobinze <yaobinze@gmail.com>
2024-02-24 01:29:01 +0800

    🐞 fix:接口跨域配置

--- vite.config.js
+++ vite.config.js
@@ -18,5 +18,28 @@ export default defineConfig({
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {}
+			}
+		},
+		// 关闭生成map文件 可以达到缩小打包体积
+		sourcemap: false,
+		// 关闭文件计算
+		reportCompressedSize: false
+	},
+	// 配置静态资源路径
+	base: './',
+	// 配置代理
+	server: {
+	  proxy: {
+	    '/proxy': {
+	      target: 'http://codercba.com:1888/api', // 代理地址
+	      changeOrigin: true, // 是否跨域
+	      rewrite: (path) => path.replace(/^\/proxy/, '') // 将 /proxy 重写为空
+	    }
+	  }
 	}
 })
