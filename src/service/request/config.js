c9f14b25b61eacb2ec016ab5676de747583ec8f7
yaobinze <yaobinze@gmail.com>
2024-02-24 20:48:09 +0800

    🐞 fix:添加env判断

--- src/service/request/config.js
+++ src/service/request/config.js
@@ -1,5 +1,7 @@
 // const BASE_URL = 'http://123.207.32.32:1888/api'
 // export const BASE_URL = 'http://codercba.com:1888/api'
 // export const BASE_URL = import.meta.env.VITE_BASE_URL
-export const BASE_URL = '/backend/'
+// export const BASE_URL = '/backend/'
+
+export const BASE_URL = import.meta.env.DEV ? 'http://codercba.com:1888/api' : '/backend/'
 export const TIMEOUT = 60000
